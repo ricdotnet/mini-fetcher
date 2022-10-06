810003211ea6c67b2d24d1580c732f2d01fa74a6
Ricardo Rocha <me@rrocha.uk>
2022-10-06 20:54:18 +0100

    * added an handler builder;

--- index.js
+++ index.js
@@ -27,23 +27,50 @@ const request = (url, options) => new Promise((resolve, reject) => {
     if (miniFetcherResponse.statusCode >= 400) {
       return reject(miniFetcherResponse);
     }
-    return resolve(miniFetcherResponse);
+
+    resolve(miniFetcherResponse);
   }).on('error', (err) => {
     reject(err);
   });
 });
 
-const get = async (url, options) => request(url, { ...options });
-const post = async (url, options) => request(url, { ...options, method: 'POST' });
-const put = async (url, options) => request(url, { ...options, method: 'PUT' });
-const patch = async (url, options) => request(url, { ...options, method: 'PATCH' });
-const _delete = async (url, options) => request(url, { ...options, method: 'DELETE' });
-const options = async (url, options) => request(url, { ...options, method: 'OPTIONS' });
-const head = async (url, options) => request(url, { ...options, method: 'HEAD' });
+const httpMethods = [
+  'get',
+  'post',
+  'put',
+  'patch',
+  'delete',
+  'options',
+  'head',
+  'trace',
+  'tunnel',
+];
+
+const buildHandlers = () => {
+  const miniFetcher = {};
+
+  for (let method of httpMethods) {
+    miniFetcher[method] = (url, options) => request(url, { ...options, method });
+  }
+
+  return miniFetcher;
+};
+
+const miniFetcher = buildHandlers();
+
+module.exports = miniFetcher;
+
+// const get = async (url, options) => request(url, { ...options });
+// const post = async (url, options) => request(url, { ...options, method: 'POST' });
+// const put = async (url, options) => request(url, { ...options, method: 'PUT' });
+// const patch = async (url, options) => request(url, { ...options, method: 'PATCH' });
+// const _delete = async (url, options) => request(url, { ...options, method: 'DELETE' });
+// const options = async (url, options) => request(url, { ...options, method: 'OPTIONS' });
+// const head = async (url, options) => request(url, { ...options, method: 'HEAD' });
 // tunnel
 // trace
 
-module.exports = {
-  get,
-  post,
-};
+// module.exports = {
+//   get,
+//   post,
+// };
