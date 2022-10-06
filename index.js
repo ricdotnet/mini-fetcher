3222978195b77c7b1b196e6f0c997cfadd86d3a4
Ricardo Rocha <me@rrocha.uk>
2022-10-06 20:05:04 +0100

    * initial lib setup

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const data = [];
+let miniFetcherResponse = {};
+
+const userAgent = {
+  'User-Agent': 'Mini-Fetcher'
+};
+
+const request = (url, options) => new Promise((resolve, reject) => {
+  if (!options.hasOwnProperty('headers')) {
+    options.headers = {};
+  }
+
+  const mergedHeaders = {
+    headers: {
+      ...options.headers, ...userAgent
+    }
+  };
+
+  http.get(url, { ...options, ...mergedHeaders }, (res) => {
+    miniFetcherResponse = res;
+
+    res.on('data', (chunk) => {
+      data.push(chunk);
+    });
+
+    res.on('end', () => {
+      miniFetcherResponse.data = data.toString();
+    });
+  }).on('close', () => {
+    if (miniFetcherResponse.statusCode >= 400) {
+      return reject(miniFetcherResponse);
+    }
+    return resolve(miniFetcherResponse);
+  }).on('error', (err) => {
+    reject(err);
+  });
+});
+
+const get = async (url, options) => request(url, { ...options });
+const post = async (url, options) => request(url, { ...options, method: 'POST' });
+const put = async (url, options) => request(url, { ...options, method: 'PUT' });
+const patch = async (url, options) => request(url, { ...options, method: 'PATCH' });
+const _delete = async (url, options) => request(url, { ...options, method: 'DELETE' });
+const options = async (url, options) => request(url, { ...options, method: 'OPTIONS' });
+const head = async (url, options) => request(url, { ...options, method: 'HEAD' });
+// tunnel
+// trace
+
+module.exports = {
+  get,
+  post,
+};
